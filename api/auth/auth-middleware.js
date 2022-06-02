e87792c1453b88ad1d7eec965170a7a028cd7de9
Visnu <vgcaisse@gmail.com>
2022-06-01 20:26:02 -0400

    wip

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,43 @@
+const User = require('../model')
+
+module.exports = {
+  checkUsernameFree,
+  checkUsernameExist,
+};
+
+async function checkUsernameFree(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (!req.body.username || !req.body.password) {
+      res.status(401).json({
+        message: 'username and password required'
+      })
+    } else if (users.length) {
+      res.status(422).json({
+        message: 'Username Taken'
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function checkUsernameExist(req, res, next) {
+  try {
+    const [user] = await User.findBy({ username: req.body.username })
+    if (!user) {
+      res.status(401).json({ message: 'Invalid credentials' })
+    } else if (!req.body.username || !req.body.password) {
+      res.status(401).json({
+        message: 'username and password required'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
